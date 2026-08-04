93f473024146ffba9b0c509182fcef2351e589ac
Marco <intensework443@gmail.com>
2026-08-04 15:32:37 +0200

    chore: added login and dashboardactivity

---
+++ app/src/main/java/com/cookie/securenotes/ui/dashboard/DashboardActivity.java
@@ -0,0 +1,13 @@
+package com.cookie.securenotes.ui.dashboard;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.cookie.securenotes.R;
+
+public class DashboardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
---
+++ app/src/main/java/com/cookie/securenotes/ui/login/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.cookie.securenotes.ui.login;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.cookie.securenotes.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
